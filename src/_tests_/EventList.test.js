bb8ba29673828ed484478629e1acf1b535538897
Mark Saville <mark.saville@outlook.com>
2023-09-10 18:21:20 -0700

    update eventlist test to use mockdata

--- src/_tests_/EventList.test.js
+++ src/_tests_/EventList.test.js
@@ -2,6 +2,7 @@
 
 import { render } from "@testing-library/react";
 import EventList from "../components/EventList";
+import { getEvents } from "../api";
 
 describe("<EventList /> component", () => {
   let EventListComponent;
@@ -13,10 +14,11 @@ describe("<EventList /> component", () => {
     expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
   });
 
-  test("renders correct number of events", () => {
-    EventListComponent.rerender(
-      <EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />
+  test("renders correct number of events", async () => {
+    const allEvents = await getEvents();
+    EventListComponent.rerender(<EventList events={allEvents} />);
+    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(
+      allEvents.length
     );
-    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
   });
 });
