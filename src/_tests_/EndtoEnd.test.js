23f89d6dc5d24d164004851233d221f51868ec4d
Mark Saville <mark.saville@outlook.com>
2023-09-17 14:35:34 -0700

    add end to end test for feature 1

--- src/_tests_/EndtoEnd.test.js
+++ src/_tests_/EndtoEnd.test.js
@@ -1,11 +1,58 @@
 import puppeteer from "puppeteer";
 
+// FEATURE 1
+
+describe("filter events by a city", () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: true,
+      slowMo: 250, // slow down by 250ms,
+      timeout: 0, // removes any puppeteer/browser timeout limitations (this isn't the same as the timeout of jest)
+    });
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+    await page.waitForSelector(".event");
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  test("show upcoming events from all cities by default", async () => {
+    // if your event's details have a different selector, use it instead of .event .details
+    const eventTitle = await page.$(".event .event_summary");
+    expect(eventTitle).toBeDefined();
+  });
+
+  test("show a list of of suggestions when searching by city", async () => {
+    await page.click("#city-search .city");
+
+    await page.type(".city", "Berlin");
+
+    const cityCount = await page.$$eval(".suggestions", (li) => li.length);
+    expect(cityCount).toBe(1);
+  });
+
+  test("select a city from the suggested list", async () => {
+    await page.click(".suggestions li");
+    const eventList = await page.$eval(
+      ".event_location",
+      (city) => city.innerText
+    );
+    expect(eventList).toBe("Berlin, Germany");
+  });
+});
+
+// FEATURE 2
+
 describe("show/hide an event details", () => {
   let browser;
   let page;
   beforeAll(async () => {
     browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       slowMo: 250, // slow down by 250ms,
       timeout: 0, // removes any puppeteer/browser timeout limitations (this isn't the same as the timeout of jest)
     });
